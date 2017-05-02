0b7ff425af548ea46f9501695948c0e0ceaed08a
Кирилл Кухаренко < >
2017-05-02 23:34:45 +0300

    individual task

---
+++ generate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+int main(int arcv, char** argv) {
+	if (arcv < 3) {
+		cerr << "Incorrect input\n";
+		return 1;
+	}
+	int n = atoi(argv[1]);
+	ofstream cout(argv[2]);
+
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		cout << '\t' << double(rand() % RAND_MAX) / double(RAND_MAX) << endl;
+	}
+	return 0;
+}
---
+++ map_script2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(int arcv, char** argv) {
+	if (arcv < 2) {
+		cerr << "Incorrect input\n";
+		return 1;
+	}
+	int n = atoi(argv[1]);
+	//int n = 10;
+	int m = 0, n1 = n;
+	while (n1 > 0) {
+		n1 /= 10;
+		m++;
+	}
+	double a;
+	double step = double(1) / n;
+	cin >> a;
+	if (a == 1) {
+		a -= step;
+	}
+	int snumb_of_steps = a / step;
+	cout << setprecision(m) << double(step*snumb_of_steps) << '-' << double(step*(snumb_of_steps + 1)) << '\t' << 1 << endl;
+	return 0;
+}
